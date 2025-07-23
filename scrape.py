5c895c466cd78d981eda9cc60b96d4ac8c535db8
thetorangi <torangisb@gmail.com>
2025-07-23 22:44:06 +0530

    Bypass CAPTCHA

--- scrape.py
+++ scrape.py
@@ -1,10 +1,15 @@
-import selenium.webdriver as webdriver
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
 
-from selenium.webdriver.firefox.service import Service     #Remove Comment If Using Firefox 
-# from selenium.webdriver.chrome.service import Service      #Remove Comment If Using Chrome 
 
-import time
+load_dotenv()
 
+SBR_WEBDRIVER = os.getenv("SBR_WEBDRIVER")
+
+''' The Below Block is for Testing Purpose
 
 def scrape_website(website):
     print("Launching Browser ....")
@@ -26,3 +31,25 @@ def scrape_website(website):
         return html
     finally:
         driver.quit()
+
+'''
+
+def scrape_website(website):
+    print("Connecting to Scraping Browser...")
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, "goog", "chrome")
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        driver.get(website)
+        print("Waiting captcha to solve...")
+        solve_res = driver.execute(
+            "executeCdpCommand",
+            {
+                "cmd": "Captcha.waitForSolve",
+                "params": {"detectTimeout": 10000},
+            },
+        )
+        print("Captcha solve status:", solve_res["value"]["status"])
+        print("Navigated! Scraping page content...")
+        html = driver.page_source
+        return html
+
+
