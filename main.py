908b075657dcea4a29d13abc95fd6b77cde554fc
K A R M A ☯️ <torangisb@gmail.com>
2025-07-23 14:13:09 +0000

    testing drivers

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from scrape import scrape_website
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Url : ")
 
 if st.button("Scrape Site"):
     st.write("Scraping Website")
+    result = scrape_website(url)
+    print(result)
---
+++ scrape.py
@@ -0,0 +1,23 @@
+import selenium.webdriver as webdriver
+
+# from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.firefox.service import Service 
+import time
+
+
+def scrape_website(website):
+    print("Launching Browser ....")
+
+    driver_path = "./geckodriver"
+    options = webdriver.FirefoxOptions()
+    driver = webdriver.Firefox(service=Service(driver_path),options=options)
+
+    try:
+        driver.get(website)
+        print("Loading Site")
+        html = driver.page_source
+        time.sleep(10)
+
+    finally:
+        driver.quit()
